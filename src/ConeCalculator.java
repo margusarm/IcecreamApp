d85fb1ec5224af44b4196d0499b53d7cc7f6ed6c
margusarm <margus.arm@gmail.com>
2022-05-01 17:23:00 +0300

    valmis esitamiseks

--- src/ConeCalculator.java
+++ src/ConeCalculator.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ConeCalculator {
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class ConeCalculator {
+    private double diameter;
+    private double height;
+
+    private double icecream;
+
+    public ConeCalculator(double diameter) {
+        this.diameter = diameter;
+    }
+
+    public void setIcecream(double icecream) {
+        this.icecream = icecream;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public double getIcecream() {
+        return icecream;
+    }
+
+    public double getRadius() {
+        return diameter / 2;
+    }
+
+    public double getVolume(){
+        return (Math.PI * Math.pow(this.getRadius(), 2) * height)/3;
+    }
+
+    public double getArea() {return Math.PI * this.getRadius() *(this.getRadius() +
+            Math.sqrt((Math.pow(this.getRadius(), 2) + Math.pow(height, 2))));}
+
+    public double getIcecreamCount () {return icecream / this.getVolume();}
+
 }
--- src/ConeCalculatorApp.java
+++ src/ConeCalculatorApp.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ConeCalculatorApp {
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+public class ConeCalculatorApp {
+    public static void main(String[] args) {
+        new ConeCalculatorApp();
+    }
+    public ConeCalculatorApp(){
+        NumberFormat formatter = new DecimalFormat("#0.00");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Selle programmiga saad teada, kui palju mahub sinu vahvlikoonuse sisse jäätist!");
+        System.out.print("Sisesta jäätisekoonuse diameeter sentimeetrites: ");
+        double diameter = input.nextDouble();
+        ConeCalculator cone = new ConeCalculator(diameter);
+        System.out.print("Kui pikk su jäätisekoonus on? Sisesta sügavus sentimeetrites: ");
+        double height = input.nextDouble();
+        System.out.print("Sisesta jäätise kogus milliliitrites: ");
+        //double icecream = input.nextDouble();
+        cone.setIcecream(input.nextDouble());
+        cone.setHeight(height);
+        System.out.println("Sellesse koonusesse mahub " + formatter.format(cone.getVolume())  + " milliliitrit jäätist");
+        System.out.println("Sinu vahvelkoonuse pindala on " + formatter.format(cone.getArea())  + " ruutsentimeetrit");
+        System.out.println(cone.getIcecream() + " milliliitrise jäätisega saab täita " + formatter.format(cone.getIcecreamCount()) + " vahvlikoonust.");
+    }
+
 }
